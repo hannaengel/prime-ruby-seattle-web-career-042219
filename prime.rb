5ebba074b264eb6de78b21a544f6b0321c9f6aaf
Hanna Engel <hengel@alumni.berklee.edu>
2019-04-12 04:47:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,11 @@
 # Add  code here!
 def prime? (n)
   dividers = [2, 3, 5, 7, 11]
-  dividers.each do |primes|
-    n == primes
+  for item in dividers
+    if n == item
+      true
+    else
+      false
+    end
   end
 end
