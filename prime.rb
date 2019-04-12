1d43ad06477d6c971b43c61647e734aff2b47cab
Hanna Engel <hengel@alumni.berklee.edu>
2019-04-12 04:51:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,5 @@
 # Add  code here!
+require 'prime'
 def prime? (n)
-  dividers = [2, 3, 5, 7, 11]
-  for item in dividers
-    if n == item
-      true
-    else
-      false
-    end
-  end
+  Prime.prime?(n)
 end
